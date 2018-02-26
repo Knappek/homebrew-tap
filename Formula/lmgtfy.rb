47a3df169948e1044764e61559e7944acecb586a
goreleaserbot <goreleaser@carlosbecker.com>
2018-02-26 23:48:34 +0100

    lmgtfy version v0.1.0

---
+++ Formula/lmgtfy.rb
@@ -0,0 +1,15 @@
+class Lmgtfy < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/Knappek/lmgtfy/releases/download/v0.1.0/lmgtfy_0.1.0_darwin_amd64.tar.gz"
+  version "0.1.0"
+  sha256 "a11c2763b08f9063d0cc1abbd3f1d4814fb39e91bfe30d1f8e0a56d8112ccfcc"
+
+  def install
+    bin.install "lmgtfy"
+  end
+
+  test do
+    
+  end
+end
